3a88db078a8d08a64519e67c358367aba38fb2bb
glopezfricker <glopezfricker@gmail.com>
2020-07-04 01:26:11 -0300

    primer commit hola mundo

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vamos a declarar variables
+            int num1 = 3;
+            float num2 = 3.54f;
+            byte num3 = 255;
+            char letra = 'A';
+            string Cadena = "Hola mundo";
+            Console.WriteLine("Hola mundo");
+            Console.WriteLine("Escribimos dos numeros: {0} // {1}", num1, num2);
+        }
+    }
+}
